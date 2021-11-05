fe8162afbac33675096e468edb8d183a00e52697
Lucas Templeton <templetl99@gmail.com>
2021-11-05 12:04:36 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// function handleTodo(todo){
+//     let p = document.createElement('p')
+//     let btn = document.createElement('button')
+//     btn.textContent = "Delete"
+//     p.textContent = todo
+//     p.appendChild(btn)
+//     document.getElementById("tasks").appendChild(p)   
+// }
+document.querySelector("form").addEventListener('submit', (event) => {
+    event.preventDefault();
+    handleTodo(event.target.newTask.value)
+    
+}
+)
+
+function handleTodo(todo){
+    let p = document.createElement("p")
+    let btn = document.createElement("button")
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = "Delete"
+    p.textContent = `${todo}  `
+    p.appendChild(btn)
+    document.getElementById("tasks").appendChild(p)
+}
+
+function handleDelete(e){
+    e.target.parentNode.remove()
+}
